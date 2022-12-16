28d8455fb9adcdf715e9d7a90c1f059c6cc5a904
sohailB <bhattisohail20@gmail.com>
2022-12-15 22:44:04 -0800

    some more changes

--- src/screens/Setting/SettingScreen.js
+++ src/screens/Setting/SettingScreen.js
@@ -1,91 +1,93 @@
-import React from 'react';
-import {Container,Box,Typography,Grid} from '@mui/material';
-import SubscriptionChart from './Components/SubscriptionChart/SubscriptionChart';
-import ButtonsScreen from './Components/ButtonsScreen/ButtonsScreen';
-import './style.css'
-function SettingScreen() {
-    return (  
-        <>
-        {/* <Box component='div' sx={{borderBottom:'1px solid #E1E1E6',height:'84px',width:'100%',}}></Box> */}
+import React from "react";
+import { Container, Box, Typography, Grid } from "@mui/material";
+import SubscriptionChart from "./Components/SubscriptionChart/SubscriptionChart";
+import ButtonsScreen from "./Components/ButtonsScreen/ButtonsScreen";
+import "./style.css";
+function SettingScreen({ handleChangeScreen }) {
+  return (
+    <>
+      {/* <Box component='div' sx={{borderBottom:'1px solid #E1E1E6',height:'84px',width:'100%',}}></Box> */}
 
-        <Container className='mainBox' maxWidth="xl">
-            <Grid container >
-                <Grid lg={6} md={6} sm={12} xs={12} item>
-                <Box
-                className="chartBox"
-            sx={{
-                
-                display:'flex',
-                alignItems:'center',
-                justifyContent:'center',
-                border:'1px solid #E1E1E6',
-                borderRadius:"4px",
-                height:'35vh',
-                flexDirection:'column',
-                padding:'10px',
-                margin:2
-            }}
+      <Container className="mainBox" maxWidth="xl">
+        <Grid container>
+          <Grid lg={6} md={6} sm={12} xs={12} item>
+            <Box
+              className="chartBox"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "1px solid #E1E1E6",
+                borderRadius: "4px",
+                height: "35vh",
+                flexDirection: "column",
+                padding: "10px",
+                margin: 2,
+              }}
             >
-                <Typography
+              <Typography
                 sx={{
-                    fontSize:'14px',
-                    color:'#545359',
-                    fontWeight:"600"
+                  fontSize: "14px",
+                  color: "#545359",
+                  fontWeight: "600",
                 }}
-                >Total Subscription</Typography>
+              >
+                Total Subscription
+              </Typography>
 
-                <SubscriptionChart/>
+              <SubscriptionChart />
             </Box>
-                </Grid>
-                <Grid lg={6} md={6} sm={12} xs={12}>
-                <Box
-                className="chartBox"
-            sx={{
-                
-                display:'flex',
-                alignItems:'center',
-                justifyContent:'center',
-                border:'1px solid #E1E1E6',
-                borderRadius:"4px",
-                height:'35vh',
-                flexDirection:'column',
-                padding:'15px',
-                margin:2
-            }}
+          </Grid>
+          <Grid lg={6} md={6} sm={12} xs={12}>
+            <Box
+              className="chartBox"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "1px solid #E1E1E6",
+                borderRadius: "4px",
+                height: "35vh",
+                flexDirection: "column",
+                padding: "15px",
+                margin: 2,
+              }}
             >
-                  <Typography
+              <Typography
                 sx={{
-                    fontSize:'14px',
-                    color:'#545359',
-                    fontWeight:"600",
-                    
+                  fontSize: "14px",
+                  color: "#545359",
+                  fontWeight: "600",
                 }}
-                >Total Meals Prepared</Typography>
-                <Typography
+              >
+                Total Meals Prepared
+              </Typography>
+              <Typography
                 sx={{
-                    fontSize:{xs:'24px',md:'32px'},
-                    fontWeight:'600',
-                    marginTop:3
+                  fontSize: { xs: "24px", md: "32px" },
+                  fontWeight: "600",
+                  marginTop: 3,
                 }}
-                >1352</Typography>
-                <Typography
+              >
+                1352
+              </Typography>
+              <Typography
                 sx={{
-                    marginTop:3,
-                    color:'#9EA3AE',
-                    fontSize:'16px',
-                    fontWeight:'400'
+                  marginTop: 3,
+                  color: "#9EA3AE",
+                  fontSize: "16px",
+                  fontWeight: "400",
                 }}
-                >Meals Plan</Typography>
+              >
+                Meals Plan
+              </Typography>
             </Box>
-
-                </Grid>
-            </Grid>
-
-           
-        </Container>
-        <ButtonsScreen/>
-        </>
-    );
+          </Grid>
+        </Grid>
+      </Container>
+      <ButtonsScreen handleChangeScreen={(id) => handleChangeScreen(id)} />
+    </>
+  );
 }
 
 export default SettingScreen;
--- src/screens/dashboard/Settings/index.js
+++ src/screens/dashboard/Settings/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import DashboardLayout from "../../DashboardLayout/DashboardLayout";
 import CustomerFeedbackDashboard from "./CustomerFeedback/CustomerFeedbackDashboard";
-
+import DashBoard from "screens/Setting/SettingScreen";
 const Settings = () => {
+  const [currentPage, setCurrentTab] = useState("dashboard");
+  const renderTab = {
+    dashboard: <DashBoard handleChangeScreen={(id) => setCurrentTab(id)} />,
+    customerFeedback: (
+      <CustomerFeedbackDashboard
+        handleBack={(id) => setCurrentTab("dashboard")}
+      />
+    ),
+  };
   return (
     <>
-      <DashboardLayout>
-          <CustomerFeedbackDashboard />
-      </DashboardLayout>
+      <DashboardLayout>{renderTab[currentPage]}</DashboardLayout>
     </>
   );
 };
